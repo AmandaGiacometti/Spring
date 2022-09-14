1e2c302502b4b3f98ce3990c43b36abbd94be6f9
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2022-09-13 23:41:06 -0300

    Concluído

---
+++ Tarefa03Crud02/tarefa2blogpessoal/src/main/java/com/generation/blogpessoal/Tarefa2blogpessoalApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefa2blogpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefa2blogpessoalApplication.class, args);
+	}
+
+}
